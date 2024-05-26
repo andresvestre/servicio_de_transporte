e43cb3ec13560e627e170e0d086c1d649ca937ba
Andres <andresvestre@hotmail.com>
2024-05-26 16:32:43 -0500

    se adiciona TypeScipt

--- index.js
+++
@@ -1,4 +0,0 @@
-const valorA = 1
-const valorB = 2
-
-console.log(valorA + valorB)
---
+++ src/api/server/main.ts
@@ -0,0 +1,8 @@
+console.log(1503)
+
+const nuevo: any = 0;
+
+function algo(s: string) {
+    console.log(s)
+}
+algo("098")
