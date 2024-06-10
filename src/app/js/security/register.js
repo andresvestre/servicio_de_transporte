42bbeab27ddac93354d72f5b5c88b5372f675fc5
Andres <andresvestre@hotmail.com>
2024-06-10 11:24:06 -0500

    Se adiciona endpoint para el registro de los usuarios solicitantes

--- src/app/js/security/register.js
+++ src/app/js/security/register.js
@@ -1,12 +1,12 @@
-const formLogin = document.querySelector('#frmLogin')
-formLogin.addEventListener('submit', handlerSubmitForm)
+const formRegister = document.querySelector('#frmRegister')
+formRegister.addEventListener('submit', handlerSubmitForm)
 
 async function handlerSubmitForm (e) {
   e.preventDefault()
-  await login()
+  await register()
 }
 
-async function login () {
+async function register () {
   const requestMessage = captureFields()
   const responseMessage = await sendMessage(requestMessage)
 
@@ -15,14 +15,18 @@ async function login () {
 }
 
 function captureFields () {
+  const tipoIdentificacionId = document.querySelector('#sltTipoIdentificacionId').value
+  const identificacion = document.querySelector('#iptIdentificacion').value
+  const nombre = document.querySelector('#iptNombre').value
+  const apellido = document.querySelector('#iptApellido').value
   const username = document.querySelector('#iptUsername').value
   const password = document.querySelector('#iptPassword').value
 
-  return { username, password }
+  return { tipoIdentificacionId, identificacion, nombre, apellido, username, password }
 }
 
 async function sendMessage (requestMessage) {
-  const responseMessage = await fetch('/api/v1/security/login', {
+  const responseMessage = await fetch('/api/v1/security/register', {
     headers: { 'content-type': 'application/json' },
     method: 'post',
     body: JSON.stringify(requestMessage)
@@ -30,4 +34,3 @@ async function sendMessage (requestMessage) {
 
   return responseMessage
 }
-
