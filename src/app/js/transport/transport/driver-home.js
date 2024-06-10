28968f02fdbebf20247883b73e612e143f33f33f
Andres <andresvestre@hotmail.com>
2024-06-10 14:23:00 -0500

    Se adiciona vista para guardar información del vehículo

---
+++ src/app/js/transport/transport/driver-home.js
@@ -0,0 +1,47 @@
+const formRegister = document.querySelector('#frmVehiculo')
+formRegister.addEventListener('submit', handlerSubmitForm)
+
+async function handlerSubmitForm (e) {
+  e.preventDefault()
+  await register()
+}
+
+async function register () {
+  const requestMessage = await captureFields()
+  const responseMessage = await updateVehicle(requestMessage)
+
+  sessionStorage.setItem('vehicle', JSON.stringify(responseMessage))
+}
+
+async function captureFields () {
+  const tipoVehiculoId = document.querySelector('#sltTipoVehiculoId').value
+  const placa = document.querySelector('#iptPlaca').value
+  const color = document.querySelector('#iptColor').value
+  const modelo = document.querySelector('#iptModelo').value
+  const asientos = document.querySelector('#iptAsientos').value
+
+  const { latitud, longitud } = await captureGps()
+
+  return { tipoVehiculoId, placa, color, modelo, asientos, latitud, longitud }
+}
+
+async function captureGps () {
+  return new Promise(resolve => {
+    navigator.geolocation.getCurrentPosition(position => {
+      resolve({
+        latitud: position.coords.latitude,
+        longitud: position.coords.longitude
+      })
+    })
+  })
+}
+
+async function updateVehicle (requestMessage) {
+  const responseMessage = await fetch('/api/v1/transport/vehicle', {
+    headers: { 'content-type': 'application/json' },
+    method: 'post',
+    body: JSON.stringify(requestMessage)
+  }).then(r => r.json())
+
+  return responseMessage
+}
